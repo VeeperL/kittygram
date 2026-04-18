199d9984519f76904617d09a42722d36e35d8f9b
Валерия <valeria@MacBook-Pro-Valeria.local>
2026-04-18 22:51:58 +0300

    Fix: nginx media paths and register Cat model

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Cat
 
-# Register your models here.
+@admin.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_year', 'owner')
